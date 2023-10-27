4f3ee7cba782ee8c8a2dfbaacb37ef1bbefcccf3
Mateen92294 <mateensayed89@gmail.com>
2023-10-27 19:30:51 +0530

    second commit

--- darklight.js
+++ darklight.js
@@ -1,41 +1,37 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
-const image1 = document.getElementById('image 1');
-const image2 = document.getElementById('image 2');
-const image3 = document.getElementById('image 3');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
+// Dark or Light Images
+function imageMode(color) {
+    image1.src = `concept_${color}.svg`;
+    image2.src = `feeling_${color}.svg`;
+    image3.src = `proud_${color}.svg`;
+  }
+
+// Dark Mode styles
 function darkMode(){
     nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     toggleIcon.children[0].textContent='Dark Mode';
-    toggleIcon.children[1].classList.remove('fa-sun');
-    toggleIcon.children[1].classList.add('fa-moon');
-    image1.src = 'img/Dark 1.svg';
-    image2.src = 'img/Dark 2.svg';
-    image3.src = 'img/Dark 3.svg';
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
     imageMode('dark');
 }
 
+// Light Mode Styles
 function lightMode(){
     nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     toggleIcon.children[0].textContent='Light Mode';
-    toggleIcon.children[1].classList.remove('fa-moon');
-    toggleIcon.children[1].classList.add('fa-sun');
-    image1.src = 'img/light 1.svg';
-    image2.src = 'img/light 2.svg';
-    image3.src = 'img/light 3.svg';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
     imageMode('light');
 }
 
-function imageMode(color) {
-    image1.src = `img/undraw_proud_coder_${color}.svg`;
-    image2.src = `img/undraw_feeling_proud_${color}.svg`;
-    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
-  }
-
+//   Switch Theme automatically
 function switchTheme(event) {
 if(event.target.checked){
     document.documentElement.setAttribute('data-theme', 'dark');
@@ -48,8 +44,10 @@ if(event.target.checked){
 }
 }
 
+// Event listener
 toggleSwitch.addEventListener('change', switchTheme);
 
+// Check Local Storage
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme){
     document.documentElement.setAttribute('data-theme', currentTheme);  
